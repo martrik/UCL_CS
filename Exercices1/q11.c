5e7cc154a78ebc6c4ccc2299cf245cb610e30b4f
Martí Serra Vivancos <martiserravivancos@gmail.com>
2015-10-09 14:55:29 +0100

    Question 11 added

--- Exercices1/q11.c
+++ Exercices1/q11.c
@@ -1,14 +1,13 @@
 #include <math.h>
 #include "graphics.h"
-#define M_PI 3.14159265358979323846
+#include <stdio.h>
 
 int main() {
   drawLine(10, 150, 400, 150);
   drawLine(20, 10, 20, 290);
-  double i = 0;
 
-  for (i; i<M_PI*2; i += 0.01){
-    drawRect((int)i/(2*M_PI)*390+10, 150+(int)sin(i)*140, 1, 1);
+  for (double i = 0; i<3.1416*2; i += 0.01){
+    drawRect((int)(i/(2*3.1416)*380)+20, 150+(int)(sin(i)*140), 1, 1);
   }
   return 0;
 }
