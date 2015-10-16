7d3c70d8976cffde4d48b6166e1c414cc0538bd1
Martí Serra Vivancos <martiserravivancos@gmail.com>
2015-10-16 15:59:18 +0100

    More exercices added

--- Exercices2/q9.c
+++ Exercices2/q9.c
@@ -2,15 +2,15 @@
 
 int main() {
   int reversing = 0;
-
   int i;
+  int rows = 0;
   for (i = 0; i<9; i++) {
-    if (i == 4) reversing = 1;
-    int rows = i+1 -reversing*(
-    printf("*");
+    if (i<5) rows++;
+    else rows--;
     int t;
+    printf("*");
     for (t = 1; t<rows; t++) {
-      if (t == rows-1) {
+      if (t == rows-1)  printf("*");
       else printf(" ");
     }
     printf("\n");
