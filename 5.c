d9c781925db9cc4057ec232424788ff40b696a1e
Marti Serra <martiserravivancos@gmail.com>
2015-11-03 16:46:59 +0000

    Improving

--- 5.c
+++ 5.c
@@ -1,51 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void copyString (char *to, char *from, int index) {
-  to += index
-  while (*from)  
-    *to++  = *from++; 
-  *to = ‘\0’; 
+  to += index;
+  while (*from) {
+    *to++  = *from++;
+  }
+  *to = '\0'; 
 }
 
 int stringLength (char *string) { 
   char *c = string; 
-  while (*c) ++c; 
+  while (*c) {
+    ++c;
+  }
   return c - string; 
 } 
 
 char** readText(char **text) {
-  text = malloc(sizeof(char*)*lines);
   int lineIndex = 0;
 
   char *line = malloc(sizeof(char)*41);
   int charIndex  = 0;
   
-  char *word;
+  char *word = malloc(sizeof(char) * 128);
   scanf("%s", word);
-  int length = stringLength(word);
+  int length =  stringLength(word);
 
   while (*word != '\0') {
-    if (countercharIndex+length <= 40) {
-      copyString(*(text+lines-1), word, counter);
-      countercharIndex += length;
+    if (charIndex+length <= 40) {
+      copyString(line+lineIndex, word, charIndex);
+      charIndex += length;
     } else {
-      *(line+40) = '\n';
+      *(line+40) = '\0';
       *(text+lineIndex) = line; // Finish previous
       lineIndex++;
       charIndex = 0;
-
+      
+      text = realloc(text, (lineIndex+1)*sizeof(char*)); // Reallocate memory to fit new line
       line = malloc(sizeof(char)*41); // Start new line
-      copyString(*(text+lines-1), word, counter);
-      countercharIndex = length;
+      copyString(line+lineIndex, word, 0);
+      charIndex = length;
     }
     scanf("%s", word);
+    length = stringLength(word);
+    printf("%d \n", length);
   }
+
   return text;
 }
 
 int main() {
-  char **text;
-
+  char **text = malloc(sizeof(char*));
   readText(text);
 
 }
