27f2cef4705afca7ef54aece2644f4844fa45dc7
Martí Serra Vivancos <martiserravivancos@gmail.com>
2015-11-04 21:12:10 +0000

    Question 5 half

--- Exercices4/5.c
+++ Exercices4/5.c
@@ -1,26 +1,60 @@
-#import <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  int counter = 0;
-  char** text;
+void copyString (char *to, char *from, int index) {
+  to += index;
+  while (*from) {
+    *to++  = *from++;
+  }
+  *to = '\0';
+}
 
-  while (counter >= 0) {
-    char *word = ;
+int stringLength (char *string) {
+  char *c = string;
+  while (*c) {
+    ++c;
+  }
+  return c - string;
+}
+
+char** readText(char **text) {
+  int lineIndex = 0;
+
+  char *line = malloc(sizeof(char)*41);
+  int charIndex  = 0;
+
+  char *word = malloc(sizeof(char) * 128);
+  scanf("%s", word);
+  int length =  stringLength(word);
+
+  while (strcmp(word, "xxx") != 0) {
+    if (charIndex+length <= 40) {
+      copyString(line+lineIndex, word, charIndex);
+      charIndex += length;
+      *(line+charIndex) = ' ';
+      charIndex++;
+    } else {
+      *(line+charIndex) = '\0';
+      *(text+lineIndex) = line; // Finish previous
+      lineIndex++;
+      charIndex = 0;
+
+      line = malloc(sizeof(char)*41); // Start new line
+      copyString(line+lineIndex, word, 0);
+      charIndex = length;
+    }
     scanf("%s", word);
-    if (word[0] == '\0') {
-      counter = -1;
-      break;
-    }
-    if (counter == 4) {
-      counter = -1;
-      break;
-    }
-    text[counter] = word;
-
-    counter++;
+    length = stringLength(word);
   }
 
-  printf("%c", text[4][1]);
+  return text;
+}
 
-  return 0;
+int main() {
+  char **text = malloc(sizeof(char*)*200);
+  readText(text);
+
+  printf("%s\n", *(text+1));
+
 }
